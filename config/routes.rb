f32f142031a802ca7e92f0f0c39d8ba3d32c5083
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-06 13:27:38 -0500

    debug session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/recipes' => 'recipes#index'
-  get '/recipes/:indgredient_name' => 'recipes#show'
+  get '/recipes/:ingredient_name' => 'recipes#show'
   post '/recipes' => 'recipes#create'
 
   get '/ingredients' => 'ingredients#index'
